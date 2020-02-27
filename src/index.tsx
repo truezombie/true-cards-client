0836c92cbbb7affa8184c91251d1ec690c5853f9
Oleg Ovcharenko <oleshka.ovcharenko@gmail.com>
2020-02-28 00:16:29 +0200

    feat: linter console.log rule

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import { IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-let gg = 1;
-
 const test = (name: string): string => {
-  console.log(name);
   return name;
 };
 
