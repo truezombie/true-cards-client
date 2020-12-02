fff1bc9a521ee174d5f1313777f8a3db05b355bd
Oleg Ovcharenko <Oleshka.Ovcharenko@gmail.com>
2020-12-02 18:07:17 +0200

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const CONFIG = {
-  backendUrl: 'http://localhost:3000/graphql',
+  backendUrl: 'http://3.122.59.81:3000/graphql',
   maxErrorMessages: 5,
   paginationRowsPerPage: [15, 30, 50],
 };
