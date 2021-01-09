1eaabe931fee5ae625c1193c26e1a2acf41c21ec
Ovcharneko Oleh <oleshka.ovcharenko@gmail.com>
2021-01-09 18:51:29 +0200

    fix: backend API link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,8 @@
 const CONFIG = {
-  backendUrl: 'http://localhost:3000/graphql',
+  backendUrl: 'http://3.122.59.81:3000/graphql',
   maxErrorMessages: 5,
   paginationRowsPerPage: [10, 30, 50],
 };
+3;
 
 export default CONFIG;
